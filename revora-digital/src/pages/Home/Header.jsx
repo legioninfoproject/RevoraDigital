6484c3ade7a85018a745fbd4638605129a7de2c8
JinoNadarajan <legioninfoproject@gmail.com>
2026-05-27 13:19:02 +0530

    meta

--- revora-digital/src/pages/Home/Header.jsx
+++ revora-digital/src/pages/Home/Header.jsx
@@ -50,10 +50,13 @@ const Header = () => {
             </h1>
 
             <p className={styles.heroDesc}>
-              Revora Digital helps businesses improve online visibility,
-              generate qualified leads, strengthen brand authority, and
-              achieve measurable growth through SEO, branding,
-              performance marketing, and conversion-focused strategies.
+             Revora Digital helps businesses improve online visibility,
+              generate qualified leads, strengthen brand authority, 
+              and achieve measurable growth through SEO, branding, performance marketing,
+               and conversion-focused digital strategies. Many businesses struggle with low website traffic, 
+               poor Google rankings, inconsistent leads, and weak online engagement. 
+               Our customized digital marketing strategies solve these challenges through SEO, branding, 
+               content marketing, and performance-driven campaigns focused on long-term growth.
             </p>
 
             <div className={styles.heroCtas}>
