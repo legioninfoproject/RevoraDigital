7b62a88ed2c725226229c1071e272d4a397da119
JinoNadarajan <legioninfoproject@gmail.com>
2026-06-01 10:37:54 +0530

    content

--- revora-digital/src/pages/Home/Header.jsx
+++ revora-digital/src/pages/Home/Header.jsx
@@ -53,10 +53,11 @@ const Header = () => {
              Revora Digital helps businesses improve online visibility,
               generate qualified leads, strengthen brand authority, 
               and achieve measurable growth through SEO, branding, performance marketing,
-               and conversion-focused digital strategies. Many businesses struggle with low website traffic, 
+               and conversion-focused digital strategies.
+                {/* Many businesses struggle with low website traffic, 
                poor Google rankings, inconsistent leads, and weak online engagement. 
                Our customized digital marketing strategies solve these challenges through SEO, branding, 
-               content marketing, and performance-driven campaigns focused on long-term growth.
+               content marketing, and performance-driven campaigns focused on long-term growth. */}
             </p>
 
             <div className={styles.heroCtas}>
